237dea0ac6cd26ddb540efab099cad80b2e2a858
gutosoethe <gutosoethe@gmail.com>
2022-05-25 16:30:03 -0300

    add botao de remover/add card

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,19 +1,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emojis = ["🚗","🚕", "🚜","🚂", "✈️","🚙","🚌", "🚎","🏎", "🚓","🚑","🚒", "🚐","🛻", "🚚"]
+    
+    @State var emojiCount = 4
+    
     var body: some View {
-        HStack{
-            CardView()
-            CardView()
-            CardView()
-            CardView()
-        }
+        VStack{
+            HStack{
+                ForEach(emojis[0..<emojiCount], id: \.self){ emoji in
+                    CardView(content: emoji)
+                }
+            }
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
+
+            }
         .padding(.horizontal)
         .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        Button(action: {
+            if emojiCount <= 1{
+                return
+            }else {
+                emojiCount -= 1
+            }
+        }, label: {
+            VStack{
+                Text("Remove")
+                Text("Card")
+            }
+        })
+    }
+    
+    var add: some View {
+        Button(action: {
+            if emojiCount >= emojis.count{
+                return
+            }else {
+                emojiCount += 1
+            }
+            
+        }, label: {
+            VStack{
+                Text("Add")
+                Text("Card")
+            }
+        })
     }
 }
 
 struct CardView: View {
+    var content: String
     @State var isFaceUp: Bool = true
     var body: some View{
         return ZStack {
@@ -21,7 +66,7 @@ struct CardView: View {
             if isFaceUp {
                 shape.fill(.white)
                 shape.stroke(lineWidth: 5)
-                Text("🚜").font(.largeTitle)
+                Text(content).font(.largeTitle)
             }else {
                 shape.fill(.red)
             }
@@ -73,8 +118,8 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
             .preferredColorScheme(.dark)
             .previewDevice("iPhone 13 mini")
-        ContentView()
-            .preferredColorScheme(.light)
-            .previewDevice("iPhone 12 mini")
+//        ContentView()
+//            .preferredColorScheme(.light)
+//            .previewDevice("iPhone 12 mini")
     }
 }
